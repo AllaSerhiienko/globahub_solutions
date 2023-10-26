56c14066957494230d754a21c51831c9e6cd9451
Alla Serhiienko <allasergienko706@gmail.com>
2023-10-26 22:55:06 +0200

    Updates

---
+++ index.84124289.js
@@ -0,0 +1,3 @@
+window.addEventListener("hashchange",()=>{"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});const e=document.querySelector("form");e.addEventListener("submit",function(t){t.preventDefault(),e.reset()});//# sourceMappingURL=index.84124289.js.map
+
+//# sourceMappingURL=index.84124289.js.map
