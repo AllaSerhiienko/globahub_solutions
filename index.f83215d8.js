231f94f0fe9a20dc984ecd279aa23006b428e3e5
Alla Serhiienko <allasergienko706@gmail.com>
2023-10-27 22:02:18 +0200

    Updates

--- index.f83215d8.js
+++ index.f83215d8.js
@@ -1,5 +1,6 @@
 window.addEventListener("hashchange",()=>{"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});const e=document.querySelector(".phone__screen");function n(){e.classList.add("show")}window.addEventListener("scroll",()=>{let o=e.getBoundingClientRect().top,t=window.innerHeight;o<t&&(n(),// Видаляємо обробник події після анімації, якщо потрібно виконати її тільки один раз
-window.removeEventListener("scroll",n))}),// Якщо елемент початково в області видимості, викликаємо анімацію відразу
-n();//# sourceMappingURL=index.26edfddf.js.map
+window.removeEventListener("scroll",n))});// Якщо елемент початково в області видимості, викликаємо анімацію відразу
+// showPhoneScreen();
+//# sourceMappingURL=index.f83215d8.js.map
 
-//# sourceMappingURL=index.26edfddf.js.map
+//# sourceMappingURL=index.f83215d8.js.map
